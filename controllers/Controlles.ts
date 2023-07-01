ecbf38cbcab8bcd27fcec34cf96daf574f0bb5de
Bibooo25730 <Bibooo@88.com>
2023-07-02 00:41:26 +0800

    控制器

--- controllers/Controlles.ts
+++ controllers/Controlles.ts
@@ -14,7 +14,7 @@ const createUpdate = (req:Request,res:Response)=>{
         res.status(400);
         throw new Error("All fields are mandatory !")
     }
-    res.status(200).json({message:`Get contact fo ${req.params.id}`})
+    res.status(200).json({message:`Get contact for ${req.params.id}`})
 }
 //@desc @put
 const createPut = (req:Request,res:Response)=>{
