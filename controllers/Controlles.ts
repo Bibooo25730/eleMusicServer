3c63c85884245605c6c9567e80c23361649e2b15
Bibooo25730 <Bibooo@88.com>
2023-07-03 23:01:26 +0800

    contactModel.ts

--- controllers/Controlles.ts
+++ controllers/Controlles.ts
@@ -1,33 +1,79 @@
-const express = require("express");
 import {Response,Request} from "express";
+const asyncHandler = require("express-async-handler")
+import Contact from "../models/contactModel";
 
-const router = express.Router();
 //@desc @get
-const createContact = (req:Request,res:Response)=>{
-    res.status(200).json({message:`Get contact for ${req.params.id}`})
-}
+
+const createContact =asyncHandler(async (req:Request,res:Response)=>{
+    try {
+        const contacts = await Contact.find();
+        console.log(contacts)
+        res.status(201).json(contacts);
+    } catch (error) {
+        console.error("查询失败", error);
+        res.status(500).json({ message: "服务器错误" });
+    }
+
+})
+
+const createGet = asyncHandler(async (req:Request,res:Response)=>{
+     const id = req.params.id
+    try {
+        const contacts = await Contact.findById(id)
+        res.status(200).json(contacts)
+    } catch (error){
+         res.status(400)
+         throw new Error("not found")
+    }
+
+})
+
+
 //@desc @update
-const createUpdate = (req:Request,res:Response)=>{
+const createUpdate = asyncHandler (async (req:Request,res:Response)=>{
     console.log(req.body)
-    const {name} = req.body
-    if(!name){
+    const {name,email,phone} = req.body
+    if(!name||!email||!phone){
         res.status(400);
         throw new Error("All fields are mandatory !")
+    }try {
+        const contact = await Contact.create({
+            name,
+            email,
+            phone
+        });
+        res.status(200).json(contact);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: '创建联系人失败' });
     }
-    res.status(200).json({message:`Get contact for ${req.params.id}`})
-}
+})
 //@desc @put
-const createPut = (req:Request,res:Response)=>{
-    res.status(200).json({message:`Get contact for ${req.params.id}`})
-}
+const createPut =asyncHandler(async (req:Request,res:Response)=>{
+    const id = req.params.id;
+    const contacts = await Contact.findById(id)
+    if(!contacts){
+        res.status(400)
+        throw  new Error("not found")
+    }
+    const updatePut = await  Contact.findByIdAndUpdate(
+        id,
+        req.body,
+        {new:true}
+    )
+    res.status(200).json(updatePut)
+})
 //@desc @delete
-const createDelete = (req:Request,res:Response)=>{
-    res.status(200).json({message:`Get contact for ${req.params.id}`})
-}
+const createDelete = asyncHandler(async (req:Request,res:Response)=>{
+    const id = req.params.id;
+    const contact = await  Contact.findByIdAndRemove(id)
+    res.status(200).json(contact)
+})
 
 module.exports = {
     createContact,
     createUpdate,
     createPut,
-    createDelete
+    createDelete,
+    createGet
 };
