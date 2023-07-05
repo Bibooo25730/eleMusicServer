ec0fa68542c0cc18fe1da4b0b8b9ebbce17db8c1
Bibooo25730 <Bibooo@88.com>
2023-07-05 12:11:58 +0800

    Contact

--- routes/musicPlay.ts
+++ routes/musicPlay.ts
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const  validateToken = require("../middleware/validateTokenHandle")
 const {
      createContact,
      createPut,
-     createUpdate,
+     createCreate,
      createDelete,
      createGet
 } = require("../controllers/Controlles")
+router.use(validateToken)
 router.route("/").get(createContact)
-router.route("/").post(createUpdate)
+router.route("/").post(createCreate)
 router.route("/:id").get(createGet).put(createPut).delete(createDelete)
 module.exports = router;
